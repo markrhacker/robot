6f1d90e38d2c12c59be726a5edd590ea638ba6d2
Mark Hacker <mark.r.hacker@gmail.com>
2018-04-25 18:25:47 +0100

    Commit message

--- camview.py
+++ camview.py
@@ -2,16 +2,28 @@ import os
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-
+import platform
 
 # --- init everything ---
 analysistype=0
+ostype=0
+
+
+if "Darwin" in platform.platform():
+    print ("macos")
+    ostype=True
+else:
+    print ("other")
+    ostype=False
+
 
 while(True):
     # --- Get image ---
     print ("Get Image...")
-    #os.system("fswebcam -q cam.jpg")
-    os.system("ffmpeg -nostats -loglevel 0 -f avfoundation -video_size 1280x720 -framerate 30 -i \"0\" -vframes 1 cam.jpg")
+    if (ostype):
+        os.system("ffmpeg -nostats -loglevel 0 -f avfoundation -video_size 1280x720 -framerate 30 -i \"0\" -vframes 1 cam.jpg")
+    else:
+        os.system("fswebcam -q cam.jpg")
     # -----------------
 
     #---- analysis imagee ----
