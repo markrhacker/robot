b9284b662835a617e708f8c074f82e59c522eab4
Mark Hacker <markhacker@Marks-MacBook-296.local>
2019-09-30 14:13:45 +0100

    Commit message

---
+++ viewer.py
@@ -0,0 +1,17 @@
+#viewer.py
+import bimpy
+from PIL import Image
+
+ctx = bimpy.Context()
+
+ctx.init(800, 800, "Image")
+
+image = Image.open("test.png")
+
+im = bimpy.Image(image)
+
+while(not ctx.should_close()):
+    with ctx:
+        bimpy.text("Display PIL Image")
+
+        bimpy.image(im)
