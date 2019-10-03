63e384854be8b2c0a1f19501190cf25f263947ed
Mark Hacker <markhacker@Marks-MacBook-296.local>
2019-10-03 20:54:14 +0100

    Commit message

--- viewer.py
+++ viewer.py
@@ -1,27 +1,44 @@
 #viewer.py
 import bimpy
+import os
 from PIL import Image
+import cv2
 
-#################################################
+#################################################\
 #init
 ctx = bimpy.Context()
-ctx.init(800, 800, "Image")
+ctx.init(700, 800, "Image")
 
 #################################################
 #get image
-
-
-
+image = Image.open("/Users/markhacker/Desktop/robot/cam.jpg")
+image = image.resize((640,350), Image.ANTIALIAS)
+#MacOS
+#brew install 
+#imagesnap -w 2 cam.jpg
+#Linux
+#sudo apt-get install fswebcam
+#fswebcam -r 1280x1024 --line-colour '#FF000000' --banner-colour '#FF000000' -F 10 cam.jpg
 
 ##################################################
 #view image
-image = Image.open("cam.jpg")
+
 
 im = bimpy.Image(image)
 
 while(not ctx.should_close()):
     with ctx:
-        bimpy.text("Display PIL Image")
-
+        bimpy.begin("Image")
         bimpy.image(im)
+        bimpy.end()
+
+        bimpy.begin("Analysis")
+        bimpy.text("Hello, world!")
+        bimpy.end()
+
+
+        bimpy.begin("Console")
+        bimpy.text("Image read")
+        bimpy.end()
+
 
