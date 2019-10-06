71410def5867bd5113749454ca1f3559d57fe8d3
Mark Hacker <markhacker@Marks-MacBook-296.local>
2019-10-06 19:54:04 +0100

    Commit message

--- viewer.py
+++ viewer.py
@@ -21,12 +21,24 @@ image = image.resize((640,350), Image.ANTIALIAS)
 #fswebcam -r 1280x1024 --line-colour '#FF000000' --banner-colour '#FF000000' -F 10 cam.jpg
 
 ##################################################
-#view image
+#look for face
+image = cv2.imread("/Users/markhacker/Desktop/robot/cam.jpg")
+cascPath = "/Users/markhacker/Desktop/robot/haarcascade_frontalface_default.xml"
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+faceCascade = cv2.CascadeClassifier(cascPath)
 
+# Detect faces in the image
+faces = faceCascade.detectMultiScale(gray, 1.1, 4)
+
+print("Found {0} faces!".format(len(faces)))
 
 ##################################################
 #UI
 im = bimpy.Image(image)
+
+f1 = bimpy.Float();
+f2 = bimpy.Float();
+f3 = bimpy.Float();
 
 while(not ctx.should_close()):
     with ctx:
@@ -36,11 +48,14 @@ while(not ctx.should_close()):
 
         bimpy.begin("Analysis")
         bimpy.text("Hello, world!")
+
+        bimpy.slider_float3("float3", f1, f2, f3, 0.0, 1.0)  
+
         bimpy.end()
 
 
         bimpy.begin("Console")
-        bimpy.text("Image read")
+        bimpy.text("Image read"+str(f1.value))
         bimpy.end()
 
 
